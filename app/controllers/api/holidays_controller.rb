db0ef452d2b937b04e4fc552768dc1e1c0fb17ed
unknown <arian.angoma@tecsup.edu.pe>
2021-08-08 01:11:36 -0500

    Get Holiday by Id

--- app/controllers/api/holidays_controller.rb
+++ app/controllers/api/holidays_controller.rb
@@ -1,11 +1,22 @@
 module Api
   class HolidaysController < ApplicationController
+    # Obtener vacaciones
     def index
       holidays = Holiday.order('created_at');
       render json: {
         status: 'OK',
         message: 'Vacaciones cargadas',
         data: holidays
+      }, status: :ok
+    end
+
+    # Obtener vacaciones por ID
+    def show
+      holiday = Holiday.find(params[:id])
+      render json: {
+        status: 'OK',
+        message: 'Vacación cargada',
+        data: holiday
       }, status: :ok
     end
   end
